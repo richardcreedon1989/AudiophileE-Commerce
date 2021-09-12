686756f887ceb6c2bb11cf32e0bf3a4a1bc1034a
richardcreedon-v1 <richard.creedon@version1.com>
2021-09-12 15:24:51 +0100

    refactored header and images

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,6 +7,12 @@ import iconLogo from "../../assets/logo.svg"
 const Header = () => {
     return (
         <nav className="header"> 
+            <ul className="desktop-header">
+                <li className="desktop-menu-items"> HOME </li>
+                <li className="desktop-menu-items"> HEADPHONES</li>
+                <li className="desktop-menu-items"> SPEAKERS </li>
+                <li className="desktop-menu-items"> EARPHONES</li>
+            </ul>
             <img className="iconBurger" src={iconBurger} alt="menu"/>
             <img className="iconCart" src={iconCart} alt="cart"/>
             <img className="iconLogo" src={iconLogo} alt="logo"/>
