6ea294b18edfff45cdfb42aebf0d8ae497492b3a
richardcreedon1989 <richardcreedon1989@gmail.com>
2021-09-22 20:35:41 +0100

    Updates

--- src/Components/CategoryTitle/CategoryTitle.js
+++
@@ -1,9 +0,0 @@
-import "./CategoryTitle.css"
-
-const className = "CategoryTitle"
-
-const CategoryTitle = ({category}) => {
-  return <div className={`${className}Heading`}> {category} </div>
-}
-
-export default CategoryTitle
